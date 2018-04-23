bbccf8b0c4299399a6ac8e0a8d301ba5e67a67dd
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ControlHighlighting/XtraReport1.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+using DevExpress.XtraPrinting;
+using DevExpress.XtraPrinting.Control;
+using DevExpress.XtraReports.UI;
+// ...
+
+namespace ControlHighlighting {
+    public partial class XtraReport1 : DevExpress.XtraReports.UI.XtraReport {
+        public XtraReport1() {
+            InitializeComponent();
+            ((PrintTool)this.ReportPrintTool).PreviewForm.PrintControl.BrickMove += new BrickEventHandler(PrintControl_BrickMove);
+        }
+
+        Brick brick;
+        void PrintControl_BrickMove(object sender, BrickEventArgs e) {
+            this.brick = e.Brick;
+            mouseMove = e.Brick != null;
+
+            ((PrintTool)this.ReportPrintTool).PreviewForm.PrintControl.Invalidate();
+            ((PrintTool)this.ReportPrintTool).PreviewForm.PrintControl.Refresh();
+        }
+
+        bool mouseMove = false;
+        private void xrLabel1_Draw(object sender, DrawEventArgs e) {
+            if (mouseMove && e.Brick == brick)
+                e.Brick.BackColor = Color.Aqua;
+            else
+                e.Brick.BackColor = Color.Transparent;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to highlight a control in Print Preview when it is being hovered by the mouse pointer
+
+
+<p>This example demonstrates the capability to change the background color of a particular control (or, to be specific, its corresponding <a href="http://documentation.devexpress.com/#WindowsForms/CustomDocument88"><u>brick</u></a>) in Print Preview when hovering over it with the mouse pointer.</p>
+
+<br/>
+
+
