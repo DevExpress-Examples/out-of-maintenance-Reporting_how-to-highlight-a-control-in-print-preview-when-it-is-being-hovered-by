c0a2b69376c4f3b70b3de34b99f98d75b5a6f291
DevExpressExampleBot <fakeEmail>
2020-09-24 14:59:29 +0300

    autogenerate VB

---
+++ VB/ControlHighlighting/XtraReport1.Designer.vb
@@ -0,0 +1,184 @@
+﻿Namespace ControlHighlighting
+	Partial Public Class XtraReport1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary> 
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.components = New System.ComponentModel.Container()
+			Dim selectQuery1 As New DevExpress.DataAccess.Sql.SelectQuery()
+			Dim column1 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression1 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim table1 As New DevExpress.DataAccess.Sql.Table()
+			Dim column2 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression2 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim column3 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression3 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim column4 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression4 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim column5 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression5 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim column6 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression6 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim column7 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression7 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim column8 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression8 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim column9 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression9 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim column10 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression10 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim column11 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression11 As New DevExpress.DataAccess.Sql.ColumnExpression()
+'INSTANT VB NOTE: The variable resources was renamed since it may cause conflicts with calls to static members of the user-defined type with this name:
+			Dim resources_Conflict As New System.ComponentModel.ComponentResourceManager(GetType(XtraReport1))
+			Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
+			Me.xrLabel2 = New DevExpress.XtraReports.UI.XRLabel()
+			Me.xrLabel1 = New DevExpress.XtraReports.UI.XRLabel()
+			Me.TopMargin = New DevExpress.XtraReports.UI.TopMarginBand()
+			Me.BottomMargin = New DevExpress.XtraReports.UI.BottomMarginBand()
+			Me.sqlDataSource1 = New DevExpress.DataAccess.Sql.SqlDataSource(Me.components)
+			CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+			' 
+			' Detail
+			' 
+			Me.Detail.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrLabel2, Me.xrLabel1})
+			Me.Detail.HeightF = 50F
+			Me.Detail.Name = "Detail"
+			Me.Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' xrLabel2
+			' 
+			Me.xrLabel2.ExpressionBindings.AddRange(New DevExpress.XtraReports.UI.ExpressionBinding() { New DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[UnitPrice]")})
+			Me.xrLabel2.Font = New System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, (CByte(204)))
+			Me.xrLabel2.LocationFloat = New DevExpress.Utils.PointFloat(489.5833F, 10.00001F)
+			Me.xrLabel2.Name = "xrLabel2"
+			Me.xrLabel2.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+			Me.xrLabel2.SizeF = New System.Drawing.SizeF(100F, 23F)
+			Me.xrLabel2.StylePriority.UseFont = False
+			Me.xrLabel2.StylePriority.UseTextAlignment = False
+			Me.xrLabel2.Text = "xrLabel2"
+			Me.xrLabel2.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleRight
+			Me.xrLabel2.TextFormatString = "{0:$0.00}"
+			' 
+			' xrLabel1
+			' 
+			Me.xrLabel1.ExpressionBindings.AddRange(New DevExpress.XtraReports.UI.ExpressionBinding() { New DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[ProductName]")})
+			Me.xrLabel1.Font = New System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, (CByte(204)))
+			Me.xrLabel1.LocationFloat = New DevExpress.Utils.PointFloat(10.00001F, 10.00001F)
+			Me.xrLabel1.Name = "xrLabel1"
+			Me.xrLabel1.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+			Me.xrLabel1.SizeF = New System.Drawing.SizeF(445.8333F, 23F)
+			Me.xrLabel1.StylePriority.UseFont = False
+			Me.xrLabel1.StylePriority.UseTextAlignment = False
+			Me.xrLabel1.Text = "xrLabel1"
+			Me.xrLabel1.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft
+'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
+'ORIGINAL LINE: this.xrLabel1.Draw += new DevExpress.XtraReports.UI.DrawEventHandler(this.xrLabel1_Draw);
+			' 
+			' TopMargin
+			' 
+			Me.TopMargin.Name = "TopMargin"
+			Me.TopMargin.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.TopMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' BottomMargin
+			' 
+			Me.BottomMargin.Name = "BottomMargin"
+			Me.BottomMargin.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.BottomMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' sqlDataSource1
+			' 
+			Me.sqlDataSource1.ConnectionName = "ControlHighlighting.Properties.Settings.nwindConnectionString"
+			Me.sqlDataSource1.Name = "sqlDataSource1"
+			columnExpression1.ColumnName = "ProductID"
+			table1.Name = "Products"
+			columnExpression1.Table = table1
+			column1.Expression = columnExpression1
+			columnExpression2.ColumnName = "ProductName"
+			columnExpression2.Table = table1
+			column2.Expression = columnExpression2
+			columnExpression3.ColumnName = "SupplierID"
+			columnExpression3.Table = table1
+			column3.Expression = columnExpression3
+			columnExpression4.ColumnName = "CategoryID"
+			columnExpression4.Table = table1
+			column4.Expression = columnExpression4
+			columnExpression5.ColumnName = "QuantityPerUnit"
+			columnExpression5.Table = table1
+			column5.Expression = columnExpression5
+			columnExpression6.ColumnName = "UnitPrice"
+			columnExpression6.Table = table1
+			column6.Expression = columnExpression6
+			columnExpression7.ColumnName = "UnitsInStock"
+			columnExpression7.Table = table1
+			column7.Expression = columnExpression7
+			columnExpression8.ColumnName = "UnitsOnOrder"
+			columnExpression8.Table = table1
+			column8.Expression = columnExpression8
+			columnExpression9.ColumnName = "ReorderLevel"
+			columnExpression9.Table = table1
+			column9.Expression = columnExpression9
+			columnExpression10.ColumnName = "Discontinued"
+			columnExpression10.Table = table1
+			column10.Expression = columnExpression10
+			columnExpression11.ColumnName = "EAN13"
+			columnExpression11.Table = table1
+			column11.Expression = columnExpression11
+			selectQuery1.Columns.Add(column1)
+			selectQuery1.Columns.Add(column2)
+			selectQuery1.Columns.Add(column3)
+			selectQuery1.Columns.Add(column4)
+			selectQuery1.Columns.Add(column5)
+			selectQuery1.Columns.Add(column6)
+			selectQuery1.Columns.Add(column7)
+			selectQuery1.Columns.Add(column8)
+			selectQuery1.Columns.Add(column9)
+			selectQuery1.Columns.Add(column10)
+			selectQuery1.Columns.Add(column11)
+			selectQuery1.Name = "Products"
+			selectQuery1.Tables.Add(table1)
+			Me.sqlDataSource1.Queries.AddRange(New DevExpress.DataAccess.Sql.SqlQuery() { selectQuery1})
+			Me.sqlDataSource1.ResultSchemaSerializable = resources_Conflict.GetString("sqlDataSource1.ResultSchemaSerializable")
+			' 
+			' XtraReport1
+			' 
+			Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail, Me.TopMargin, Me.BottomMargin})
+			Me.ComponentStorage.AddRange(New System.ComponentModel.IComponent() { Me.sqlDataSource1})
+			Me.DataMember = "Products"
+			Me.DataSource = Me.sqlDataSource1
+			Me.Version = "20.1"
+			CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+		End Sub
+
+		#End Region
+
+		Private Detail As DevExpress.XtraReports.UI.DetailBand
+		Private TopMargin As DevExpress.XtraReports.UI.TopMarginBand
+		Private BottomMargin As DevExpress.XtraReports.UI.BottomMarginBand
+		Private xrLabel2 As DevExpress.XtraReports.UI.XRLabel
+		Private WithEvents xrLabel1 As DevExpress.XtraReports.UI.XRLabel
+		Private sqlDataSource1 As DevExpress.DataAccess.Sql.SqlDataSource
+	End Class
+End Namespace
